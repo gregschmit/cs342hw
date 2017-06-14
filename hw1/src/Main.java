2856b7e3d4dca11a9f2439cf99db2e70c9ea7399
Peter Snyder <psnyde2@uic.edu>
2017-06-14 17:38:26 -0500

    add skeleton code for hw1

--- hw1/src/Main.java
+++ hw1/src/Main.java
@@ -1,5 +1,44 @@
+/**
+ * @file
+ * Skeleton code for the first homework assignment for CS 342.
+ */
+
+// You can import any libraries from the standard library you'd like.  No
+// third party code is allowed.
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+
+    System.out.println("Just about to get started…");
+
+    // First, write some code below that prints your email address out to
+    // the commandline / standard out
+
+    // Second, create a variable, called `qTwoInt` of type `int` with
+    // the value of 3.  Then, create a second variable, `qTwoInteger`, of type
+    // `Integer`, using the value of the `qTwoInt` variable.  Then, create
+    // a variable called `qTwoString`, of type `String`, that contains the
+    // string value of this integer, as converted from the `qTwoInteger`
+    // (e.g. do *not* hard code the value of `qTwoString` into your program).
+    // Print out the string value.
+
+    // Third, define an array of Strings below, containing each part of your
+    // name.  For example, if Mary Tyler Moore was completing this assignment,
+    // she'd want an array of length three, containing the strings "Mary",
+    // "Tyler" and "Moore".
+
+    // Then, iterate over this array, and print your name back out, but
+    // with "_" (underscores) separating each part of your name.  In the case
+    // of Mary Tyler Moore, the string "Mary_Tyler_Moore" would be printed.
+    // Write code that accomplishes this below.
+
+    // Fourth, write code that finds the current date, and prints the date out
+    // in the following format: YYYY-MM-DD
+
+    // Fifth, write code below that looks up and prints out the current version
+    // of Java.  This should look something like 1.8.0_131 (though will
+    // of course differ slightly depending on your setup).  Tip: If you're using
+    // the right method(s), you will not need to do any formatting to get
+    // your solution to match.
   }
 }
