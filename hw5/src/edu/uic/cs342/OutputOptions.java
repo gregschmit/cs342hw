38de80cdbe0bb01c51fe7146f199d3637022b4db
Peter Snyder <psnyde2@uic.edu>
2017-07-08 19:27:59 -0500

    cs342: initial hw5 code

---
+++ hw5/src/edu/uic/cs342/OutputOptions.java
@@ -0,0 +1,10 @@
+package edu.uic.cs342;
+
+/**
+ * Enum class that specifies whether the program should print sentences on
+ * their own lines, or as a single JSON string.
+ */
+public enum OutputOptions {
+  JSON,
+  TEXT
+}
