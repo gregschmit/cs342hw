4ec8b0e3508fee44e305c1e0aa2e433c99a304c9
Peter Snyder <psnyde2@uic.edu>
2017-07-09 23:19:01 -0500

    hw5: adding initial draft of HW5

--- hw5/tests/SentenceParserTest.java
+++ hw5/tests/SentenceParserTest.java
@@ -1,4 +1,5 @@
 import edu.uic.cs342.SentenceParser;
+import edu.uic.cs342.SentenceParserException;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -7,13 +8,23 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Collectors;
-import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Test;
 
 public class SentenceParserTest {
 
   static Charset TEST_CHARSET = StandardCharsets.UTF_8;
 
+  /**
+   * Reads a file into a String, normalizing the line endings to unix endings.
+   *
+   * @param pathName String The path to file on disk, in the tests/data
+   *                        subdirectory
+   *
+   * @return The contents of file at the given path, as a String.
+   *
+   * @throws IOException If there is an error reading the file from disk.
+   */
   static String fileToString(String pathName) throws IOException {
     Path filePath = FileSystems.getDefault().getPath("tests", "data", pathName);
     return Files.readAllLines(filePath, SentenceParserTest.TEST_CHARSET)
@@ -22,25 +33,47 @@ public class SentenceParserTest {
         .collect(Collectors.joining("\n"));
   }
 
-  static String fileToTestResult(String pathName) throws IOException {
+  /**
+   * Turns a list of strings into a single string, by joining with "\n".
+   *
+   * @param inputList List&lt;String&gt; A list of strings.
+   *
+   * @return The strings in the provided list, joined with unix new line
+   *         characters.
+   */
+  static String listToString(List<String> inputList) {
+    return inputList.stream().collect(Collectors.joining("\n"));
+  }
 
-    Path filePath = FileSystems.getDefault().getPath("tests", "data", pathName);
-    String fileContents = Files.readAllLines(filePath, SentenceParserTest.TEST_CHARSET)
-        .stream()
-        .map(x -> x.trim())
-        .collect(Collectors.joining("\n"));
+  /**
+   * Runs a test text through the SentenceParser class, joined as a String.
+   *
+   * @param pathName String The path to file on disk, in the tests/data
+   *                        subdirectory
+   *
+   * @return The result of parsing the test text through the SentenceParser,
+   *         returned as a single string, with each given sentence on
+   *         a new line.
+   *
+   * @throws IOException If there is an error reading the file from disk.
+   *
+   * @throws SentenceParserException Thrown when some error occured during
+   *                                 parsing a text into sentences.
+   */
+  static String fileToTestResult(String pathName) throws IOException, SentenceParserException {
+
+    String fileContents = SentenceParserTest.fileToString(pathName);
 
     SentenceParser parser = new SentenceParser();
     List<String> testResult = parser.asList(fileContents);
     return SentenceParserTest.listToString(testResult);
   }
 
-  static String listToString(List<String> inputList) {
-    return inputList.stream().collect(Collectors.joining("\n"));
-  }
-
+  /**
+   * Simplest test case, with sentences ending with "."s.
+   */
   @Test
-  public void toSentencesExample1Test() throws IOException {
+  public void toSentencesExample1Test() throws IOException, SentenceParserException {
 
     String correctResult = SentenceParserTest.fileToString("1-correct.txt");
     String testResult = SentenceParserTest.fileToTestResult("1-test.txt");
